cadcb563525b8e9c39eddabb8fa7502ecdd8757b
vermax <vermaxos@outlook.com>
2022-09-03 06:52:26 +0000

    Update docker-install.sh

--- installation/docker-install.sh
+++ installation/docker-install.sh
@@ -14,6 +14,8 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
+sudo chmod 666 /var/run/docker.sock
+
 sudo usermod -aG docker $USER
 
 sudo apt install docker-compose -y
